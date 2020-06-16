cef185f157f6bb6c1f9da03b7be6d93fa045fc50
hschoi4448 <48243030+hschoi4448@users.noreply.github.com>
2020-06-16 21:31:27 +0900

    .

---
+++ CMakeProject1/Formula.h
@@ -0,0 +1,9 @@
+﻿#ifndef EXAMPLEPROJECT_FORMULA_H
+#define EXAMPLEPROJECT_FORMULA_H
+
+class Formula {
+public:
+    static int bla(int arg1);
+};
+
+#endif //EXAMPLEPROJECT_FORMULA_H
---
+++ CMakeProject1/main.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+#include "Formula.h"
+
+int main() {
+    std::cout << "Bla: " << Formula::bla(2) << std::endl;
+    return 0;
+}
